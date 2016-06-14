ae6a844a415272b0524b5f3e6d286ecbb05d86b9
Daniel Burton <dpburton@xactware.com>
2016-06-14 13:58:50 -0600

    New project for iOS app

---
+++ iOS/SwissTournament/SwissTournament/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwissTournament
+//
+//  Created by Daniel Burton on 6/14/16.
+//  Copyright © 2016 Daniel Burton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
